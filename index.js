5de37d1052d55882df6b1322405dabe384f96e1d
Jordan <97803504+jojoparra@users.noreply.github.com>
2022-04-15 21:06:37 -0500

    Added
    Added objects to the array, render function, a date picker, and a textbox

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-let todo1 = 'Get groceries'; //strings
-let todo2 = 'Wash car';
-let todo3 = 'Make dinner';
 
-let todos = ['Get groceries', 'Wash car', 'Make dinner']; //array : list of values
-todos.push('another todo'); //add item to an array
+const todos = [{ // made objects in the array 
+    title: 'Get groceries',
+    dueDate: '2022-10-04'
+}, {
+    title: 'Wash car',
+    dueDate: '2022-13-04'
+}, {
+    title: 'Make dinner',
+    dueDate: '2022-14-04'
+}];
 
-//create a new variable called todoTitle
-function addTodo(todoTitle) { // create a function to resuse the code, takes a parameter
-    let element = document.createElement('div'); //create a html element of type div
-    element.innerText = todoTitle; //change the text inside the html element (div) we just created
-    document.body.appendChild(element); //adds an element to the end of the html body
+render(); // going to run the above array and render those 3 elements to the webpage
+
+function addTodo() { //whnen button is clicked, this will add a new element to the array 
+    const textbox = document.getElementById('todo-title'); //get html element by id
+    const title = textbox.value; // .value gets whatever the user typed into the textbox
+
+    const datePicker = getElementById('date-picker'); // this will grab the date we chose and store it in the variable datePicker
+    const dueDate = datePicker.value; // we assign the date the user picked to a dueDate
+    todos.push({
+        title: title,
+        dueDate: dueDate
+    }); // push the new element to the end of the list 
+
+    render(); // then this will render the updated array list to the webpage
 }
 
-addTodo(todo1); //assign the value to todoTitle
-addTodo(todo2);
-addTodo(todo3);
+
+function render() { // this is the function instructions for render 
+    //reset the list to empty before we render it again
+    document.getElementById('todo-list').innerHTML = ''; //this will wipe out the list and replace it with an empty string
+
+
+    todos.forEach(function (todo) { // this goes through each todo and renders it on the webpage
+        const element = document.createElement('div'); //create a html element of type div
+        element.innerText = todo.title + ' ' + todo.dueDate; //change the text inside the html element (div) we just created
+        const todoList = document.getElementById('todo-list');
+        todoList.appendChild(element); //adds an element to the end of the html body
+    });
+}
